c1a4a1d2e437d09cd987d2d2505c231ef530ecd2
Juan Folgar Colomer <folgarcolomer@gmail.com>
2019-08-14 08:16:01 +0100

    Companies and Employees crud.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,10 +16,11 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
+
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
